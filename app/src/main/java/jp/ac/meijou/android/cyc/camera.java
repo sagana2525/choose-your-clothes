e8f80f7f977c7cbe3035334d90e95cef3d56f7d4
Shuya Muramatsu <237186723+Muramatsu-Shuya@users.noreply.github.com>
2026-09-04 11:07:36 +0900

    テストコミット コミット確認 プッシュ確認

---
+++ app/src/main/java/jp/ac/meijou/android/cyc/camera.java
@@ -0,0 +1,29 @@
+package jp.ac.meijou.android.cyc;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import jp.ac.meijou.android.cyc.databinding.ActivityPhotoListBinding;
+
+public class camera extends AppCompatActivity {
+
+    private ActivityPhotoListBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_camera);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
