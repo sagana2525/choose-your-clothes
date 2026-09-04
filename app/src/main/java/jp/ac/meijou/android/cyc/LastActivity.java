9e936c26b343df105c70862f5a6834f9b82cc144
251205195 <251205195@ccmailg.meijo-u.ac.jp>
2026-09-04 15:01:33 +0900

    最後の画面の追加

---
+++ app/src/main/java/jp/ac/meijou/android/cyc/LastActivity.java
@@ -0,0 +1,29 @@
+package jp.ac.meijou.android.cyc;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import jp.ac.meijou.android.cyc.databinding.ActivityLastBinding;
+
+public class LastActivity extends AppCompatActivity {
+
+    private ActivityLastBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityLastBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
